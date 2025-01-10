1458dfbc2b96f5b11dfb1a19e529ae95dc361eb3
Jeff Brownlee <jbrownlee@dma.org>
2025-01-10 17:59:05 -0600

    initial type data

---
+++ data.sql
@@ -0,0 +1,9 @@
+--
+-- Data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+INSERT INTO `type` VALUES (1,'phone'),(2,'email'),(3,'address');
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
