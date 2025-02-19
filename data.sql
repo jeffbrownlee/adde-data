683288aff300cc131a16b8ef1faa5f1753fff7ba
Jeff Brownlee <jbrownlee@dma.org>
2025-02-19 13:21:58 -0600

    data model and content update

--- data.sql
+++ data.sql
@@ -2,8 +2,12 @@
 -- Data for table `type`
 --
 
+USE `adde` ;
+
 LOCK TABLES `type` WRITE;
+
 /*!40000 ALTER TABLE `type` DISABLE KEYS */;
-INSERT INTO `type` VALUES (1,'phone'),(2,'email'),(3,'address');
+INSERT INTO `type` VALUES (1,'phone'),(2,'email'),(3,'address'),(4,'name'),(5,'date of birth'),(6,'phone'),(7,'email'),(8,'social'),(9,'address'),(10,'company'),(11,'title');
 /*!40000 ALTER TABLE `type` ENABLE KEYS */;
+
 UNLOCK TABLES;
